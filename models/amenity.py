cdb5431358f8439e81192c6f16ab0e6c82281966
felixokolo <pheelix.info@gmail.com>
2022-09-15 05:31:51 +0100

    correction

--- models/amenity.py
+++ models/amenity.py
@@ -19,8 +19,9 @@ class Amenity(BaseModel, Base):
 
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity,
-                                   backref='amenities')
+    if models.storage_t == 'db':
+        place_amenities = relationship("Place", secondary=place_amenity,
+                                       backref='amenities')
 
     def __init__(self, *args, **kwargs):
         """User class init
