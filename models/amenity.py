8c1358e29d30e2eb51d2f04242ba8de96117373a
felixokolo <pheelix.info@gmail.com>
2022-09-15 04:44:12 +0000

    Revert "additions"
    This reverts commit 58ca08a3cb89831d1b47af927e72b49b81ccc652.

--- models/amenity.py
+++ models/amenity.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-from sqlalchemy import Column, Integer, String, Float
-from sqlalchemy.orm import relationship
-from sqlalchemy import ForeignKey
-from models.base_model import Base
-from models.place import place_amenity
 
 
-class Amenity(BaseModel, Base):
+class Amenity(BaseModel):
     """Amenity  class
     Attributes:
         name (str): Amenity  name
     """
 
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity,
-                                   backref='amenities')
+    name = ""
 
     def __init__(self, *args, **kwargs):
         """User class init
